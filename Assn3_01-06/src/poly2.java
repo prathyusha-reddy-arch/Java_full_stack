19dd4b2b23db711e361ea85f8e15a40ccb2dae5f
prathyusha-reddy-arch <yanalaprathyusha@gmail.com>
2023-06-02 12:31:20 -0500

    commit

---
+++ Assn3_01-06/src/poly2.java
@@ -0,0 +1,14 @@
+class poly1 {
+
+    public void poly()
+    {
+        System.out.println("in poly1");
+    }
+}
+
+public class poly2 extends poly1{
+    public void poly()
+    {
+        System.out.println("in poly2");
+    }
+}
