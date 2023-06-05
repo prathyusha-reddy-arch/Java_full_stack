0385f614e5315a0a74c8b735e5eae928f8e7bfee
prathyusha-reddy-arch <yanalaprathyusha@gmail.com>
2023-06-05 00:38:34 -0500

    commit

---
+++ javabackend/src/com/java/assn31/Student.java
@@ -0,0 +1,18 @@
+package com.java.assn31;
+
+import java.util.Scanner;
+public class Student {
+    String name="Prathyusha";
+    public  String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+
+    }
+
+
+}
+
